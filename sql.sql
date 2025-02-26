883abda4d947f3edff0268adc0bb79ccc1d22acd
aylinaslan <98816021+aylinaslan@users.noreply.github.com>
2025-02-26 09:00:03 +0300

    2. ÖDEV

--- sql.sql
+++ sql.sql
@@ -1,13 +1,9 @@
-SELECT last_name FROM customer
-WHERE first_name = 'Mary';
+SELECT*FROM film
+Where replacement_cost Between 12.99 AND 16.99;
 
-SELECT * FROM film
-Where rental_rate= 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
+SELECT first_name, last_name From actor
+Where first_name IN ('Penelope', 'Nick', 'Ed');
 
-SELECT * FROM film
-Where length>60 AND length<75;SELECT title,description FROM film;
 
-SELECT title,description FROM film;
-
-SELECT *FROM film
-Where NOT length= 50 AND NOT (rental_rate=2.99 OR rental_rate = 4.99);
+SELECT*FROM film
+Where rental_rate IN (0.99 , 2.99,4.99 ) AND replacement_cost  IN (12.99,15.99,28.99);
