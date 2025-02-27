fc3889a6b4f1320b2bba4f7d9e08bf1c68b3a233
aylinaslan <98816021+aylinaslan@users.noreply.github.com>
2025-02-27 09:15:15 +0300

    ödev4

---
+++ sql4.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT replacement_cost  FROM film;
+
+SELECT COUNT (DISTINCT replacement_cost)  FROM film;
+
+SELECT *FROM film
+WHERE title LIKE 'T%' AND rating = 'G';
+
+SELECT * FROM country
+where country LIKE '_____';
+
+SELECT * FROM city 
+WHERE city ILIKE '%R';
+
