0f2e35867d144e72dbd1e9f458b5f950eb1451a0
aylinaslan <98816021+aylinaslan@users.noreply.github.com>
2025-02-28 08:45:38 +0300

    5.ödev

---
+++ sql5.sql
@@ -0,0 +1,12 @@
+
+SELECT title, length FROM film
+WHERE title LIKE '%n' ORDER BY length DESC
+LIMIT 5;
+
+SELECT title, length
+FROM film
+WHERE title LIKE '%n'
+  AND length IN (6, 7, 8, 9, 10)
+ORDER BY length ASC;
+
+ SELECT * FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
